5e0c8cd51241fcf27de77f4859b134fe8cc2da67
Zain Bawa <zain@Zains-MacBook-Pro.local>
2018-05-03 16:05:09 +0800

    working signIn and functions of adding, completing and removing goals.

--- goalCoach/src/firebase.js
+++ goalCoach/src/firebase.js
@@ -1,13 +1,15 @@
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
 
-const config = {
-  apiKey: "AIzaSyDO9Fnyf1YOwsZJ46RshxAEYbZJLNa-19w",
-  authDomain: "goalcoach-6f23e.firebaseapp.com",
-  databaseURL: "https://goalcoach-6f23e.firebaseio.com",
-  storageBucket: "goalcoach-6f23e.appspot.com",
-  messagingSenderId: "568769933089"
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBTDEGY3DM0dQZudzpzv-_D58rkMLxe9Mw",
+  authDomain: "goalco-ee56c.firebaseapp.com",
+  databaseURL: "https://goalco-ee56c.firebaseio.com",
+  projectId: "goalco-ee56c",
+  storageBucket: "goalco-ee56c.appspot.com",
+  messagingSenderId: "308153014460"
 };
 
 export const firebaseApp = firebase.initializeApp(config);
-export const goalRef = firebase.database().ref('goals');
-export const completeGoalRef = firebase.database().ref('completeGoals');
+export const goalRef = firebase.database().ref("goals");
+export const completeGoalRef = firebase.database().ref("completeGoals");
